feb0692f6530ea0532f1a83962153b3863a551f5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-03-31 18:35:32 +0000

    minor update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class Shin extends StatelessWidget {
             trew= "Access Denied";
 
           },
-          child: Text("find"),
+          child: Text("Find"),
         ),
       appBar: AppBar(
 
